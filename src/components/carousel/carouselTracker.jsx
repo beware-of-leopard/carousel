46303ce99127062446be4b94e2abe9a5bd2e2cfc
DANIEL REVO <DANIELREVO08@GMAIL.COM>
2019-05-30 19:43:38 -0400

    Update logic to use array keys method

--- src/components/carousel/carouselTracker.jsx
+++ src/components/carousel/carouselTracker.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 
 export const CarouselTracker = (props) => {
   const { numberOfTrackers, activeCard } = props;
+  
   const getTrackerIndicators = () => {
-    let trackers = [];
-
-    for (var i = 1; i <= numberOfTrackers; i++) {
-      trackers.push(
+    const trackers = [...Array(numberOfTrackers).keys()].map((n, i) => {
+      return (
         <div 
-          className={`tracker ${activeCard === i-1 ? 'active' : ''}`}
+          className={`tracker ${activeCard === i ? 'active' : ''}`}
           key={`tracker-${i}`}>
         </div>
-      );
-    }
+      )
+    })
     return trackers;
   }
 
